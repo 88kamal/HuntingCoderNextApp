a32b0c6f3da1348edec9104bb338d0b55fc6dc49
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-09-09 21:56:33 +0530

    okk

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -8,7 +8,7 @@ function TopNav() {
            <div> <nav className={styles.mainnav}>
                 <ul className={styles.nav}>
                     <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About Us</Link></li>
+                    <li><Link href="/about">About</Link></li>
                     <li><Link href="/blog">Blog</Link></li>
                     <li><Link href="/contact">Contact</Link></li>
                 </ul>
--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,24 @@ export default function Home() {
           <Image className={styles.myImg} src="/logo.avif" width={150} height={150} />
         </div>
         <h1 className={styles.title}>
-          <h6 className='dummy'>&lt;Kamal Nayan Upadhyay/&gt;</h6>
+          <p  className= {styles.name}>&lt;Kamal Nayan Upadhyay/&gt;</p>
           <span className='dummy'>&lt;HuntingCoder/&gt;</span>
         </h1>
 
         <div className=' container my-3'>
+          <hr />
+          <h1 className=' text-center fw-bolder'>Popular Blogs</h1>
+          <hr />
+          <div className=' my-5'>
           <h5>How To Learn HTML</h5>
-          <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+          <p className=' fw-light' style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
           <h5>How To Learn JavaScript</h5>
-          <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+          <p className=' fw-light' style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
           <h5>How To Learn NextJs</h5>
-          <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+          <p className=' fw-light' style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
           <h5>How To Learn ReactJs</h5>
-          <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+          <p className=' fw-light' style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
@@ -45,3 +50,53 @@ export default function Home() {
     </div>
   )
 }
+
+
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+// import Image from 'next/image'
+
+
+// export default function Home() {
+
+//   // console.log(styles)
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Hunting Coder</title>
+//         <meta name="description" content="Generated by create next app" />
+//         <meta name="keywords" content="nextjs, huntingcoder blog, hunting coder" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       {/* <Script src='/sc.js' strategy="lazyOnload"></Script> */}
+
+//       <main className={styles.main}>
+//         <div className={styles.imagewrap}>
+//           <Image className={styles.myImg} src="/logo.avif" width={150} height={150}/>
+//           {/* <img className={styles.myImg} src="/1.avif" width={237} height={158} alt="hunting coder" /> */}
+//         </div>
+//         <h1 className={styles.title}>
+//           <h6 className='dummy'>&lt;Kamal Nayan Upadhyay/&gt;</h6>
+//           <span className='dummy'>&lt;HuntingCoder/&gt;</span>
+//         </h1>
+
+
+//         {/* <div className={`${styles1.con} ${styles2.con}`}> */}
+//         <div className=' container my-3'>
+//           <h5>How To Learn HTML</h5>
+//           <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+//           <h5>How To Learn JavaScript</h5>
+//           <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+//           <h5>How To Learn NextJs</h5>
+//           <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+//           <h5>How To Learn ReactJs</h5>
+//           <p style={{ textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam sint, voluptates, mollitia soluta quaerat assumenda obcaecati ducimus, impedit sed dolor exercitationem voluptas iusto quia quae placeat similique dignissimos labore cumque eveniet aliquid itaque ab! Ad iure in fuga recusandae magnam voluptas esse ullam? Deleniti cupiditate tempore quas nemo! Suscipit error saepe iste sequi architecto ipsa assumenda, sit amet numquam dignissimos nesciunt quis eum fugit, consequatur officia rem ex non sunt! Deleniti in provident id blanditiis dolorem doloremque quae accusamus porro enim autem voluptatum natus quia totam error non nihil inventore, quas voluptatem dignissimos deserunt! Et, enim hic. Alias, dolores.</p>
+//         </div>
+//       </main>
+
+//       <footer className={styles.footer}>
+
+//       </footer>
+//     </div>
+//   )
+// }
